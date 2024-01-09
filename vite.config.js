25a6895ecefc5d64fd8d9cba48dd39389b86ec0a
Jeremy Karlsson <karlsson@jeremy.se>
2024-01-09 23:34:32 +0100

    Ignore built folder and configure vite better

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,24 @@
-// vite.config.js
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'src/index.js')
-    }
-  },
+	build: {
+		lib: {
+			entry: resolve(__dirname, "src/index.js"),
+			formats: ["es"],
+			fileName: "index",
+		},
+		target: "es2022",
+		minify: "esbuild",
+	},
+	plugins: [
+		dts({
+			compilerOptions: {
+				checkJs: true,
+				allowJs: true,
+			},
+		}),
+	],
 });
